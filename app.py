294bb0e5b64f0bb1a50e1e9ffc7a2347e731586f
Ja7L <157943852+Ja7L@users.noreply.github.com>
2024-02-22 18:01:11 -0800

    Remove use for sessions in app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request
 from flask_session import Session
 
 # Configure application
 app = Flask(__name__)
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 @app.after_request
 def after_request(response):
