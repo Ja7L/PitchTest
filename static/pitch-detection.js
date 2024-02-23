4933459146670a44404233b1cd8a1fab5735e594
Ja7L <157943852+Ja7L@users.noreply.github.com>
2024-02-22 18:42:29 -0800

    Update pitch-detection.js

--- static/pitch-detection.js
+++ static/pitch-detection.js
@@ -1,130 +1,3 @@
-
-function init() {
-    var source;
-    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    var analyser = audioContext.createAnalyser();
-    analyser.minDecibels = -100;
-    analyser.maxDecibels = -10;
-    analyser.smoothingTimeConstant = 0.85;
-    if (!navigator?.mediaDevices?.getUserMedia) {
-        // No audio allowed
-        alert('getUserMedia is required')
-        return;
-    } else {
-        var constraints = { audio: true };
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(
-                function (stream) {
-                    // Initialize the SourceNode
-                    source = audioContext.createMediaStreamSource(stream);
-                    // Connect the source node to the analyzer
-                    source.connect(analyser);
-                    visualize();
-                }
-            )
-            .catch(function (err) {
-                alert('Microphone Required');
-            });
-    }
-  
-    // Visualizing, copied from voice change o matic
-    var canvas = document.querySelector('.visualizer');
-  
-    var canvasContext = canvas.getContext("2d");
-  
-    var WIDTH;
-    var HEIGHT;
-  
-    function visualize() {
-        WIDTH = canvas.width;
-        HEIGHT = canvas.height;
-  
-        const drawFrequency = () => {
-            const bufferLengthAlt = analyser.frequencyBinCount;
-            const dataArrayAlt = new Uint8Array(bufferLengthAlt);
-  
-            canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
-  
-            const drawAlt = () => {
-                const drawVisual = requestAnimationFrame(drawAlt);
-  
-                analyser.getByteFrequencyData(dataArrayAlt);
-  
-                canvasContext.fillStyle = 'rgb(255, 255, 255)';
-                canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
-  
-                const barWidth = (WIDTH / bufferLengthAlt) * 2.5;
-                let x = 0;
-  
-                for (let i = 0; i < bufferLengthAlt; i++) {
-                    const barHeight = dataArrayAlt[i];
-  
-                    // Change color scheme
-                    let red, green;
-  
-                    if (barHeight < 128) {
-                        red = 255;
-                        green = barHeight;
-                    } else {
-                        red = 255 - barHeight;
-                        green = 255;
-                    }
-                    let blue = 100;
-  
-                    canvasContext.fillStyle = `rgb(${red}, ${green}, ${blue})`;
-                    canvasContext.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
-  
-                    x += barWidth + 1;
-                }
-            };
-  
-            drawAlt();
-        };
-  
-        drawFrequency();
-  
-        // Thanks to PitchDetect: https://github.com/cwilso/PitchDetect/blob/master/js/pitchdetect.js
-        var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-        function noteFromPitch(frequency) {
-            // Calculate the note number based on the frequency
-            var noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
-  
-            // Round the note number and add the MIDI note number offset
-            var midiNoteNum = Math.round(noteNum) + 69;
-  
-            return midiNoteNum;
-        }
-  
-  
-        const drawNote = () => {
-            const drawVisual = requestAnimationFrame(drawNote);
-            const bufferLength = analyser.fftSize;
-            const buffer = new Float32Array(bufferLength);
-            analyser.getFloatTimeDomainData(buffer);
-            const pitch = autoCorrelate(buffer, audioContext.sampleRate);
-  
-            let pitchDisplay = 'Too quiet...';
-            let noteDisplay = 'Too quiet...';
-            if (pitch !== -1) {
-                // Round to nearest Hz
-                const roundedPitch = Math.round(pitch);
-                pitchDisplay = `Pitch: ${roundedPitch} Hz`;
-  
-                // Get closest note
-                const noteIndex = noteFromPitch(pitch) % 12;
-                noteDisplay = `Note: ${noteStrings[noteIndex]}`;
-            }
-  
-            document.getElementById('pitch').innerText = pitchDisplay;
-            document.getElementById('note').innerText = noteDisplay;
-        };
-  
-  
-        drawNote();
-    }
-  }
-  
-  
   /*
   The MIT License (MIT)
   
@@ -142,12 +15,134 @@ function init() {
   
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  FITNESS FOR A PARTICULAR PURPOSE AND NONIvNFRINGEMENT. IN NO EVENT SHALL THE
   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
   */
+
+
+  function start() {
+    // noteFromPitch & centsOffFromPitch from https://github.com/cwilso/PitchDetect/pull/23
+    var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+    function noteFromPitch(frequency) {
+        var noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
+        var midiNoteNum = Math.round(noteNum) + 69;
+        return midiNoteNum;
+    }
+
+    function centsOffFromPitch(frequency, note) {
+        return Math.floor(1200 * Math.log(frequency / frequencyFromNoteNumber(note)) / Math.log(2));
+    }
+
+    var mic;
+    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    var analyser = audioContext.createAnalyser();
+    analyser.minDecibels = -100;
+    analyser.maxDecibels = -10;
+    analyser.smoothingTimeConstant = 0.85;
+    if (!navigator?.mediaDevices?.getUserMedia) {
+        // No audio allowed
+        alert('getUserMedia is required')
+        return;
+    } else {
+        var constraints = { audio: true };
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(
+                function (stream) {
+                    mic = audioContext.createMediaStreamSource(stream);
+                    mic.connect(analyser);
+                    visualize();
+                }
+            )
+            .catch(function (err) {
+                alert('Microphone Required');
+            });
+    }
+
+    var canvas = document.querySelector('.visualizer');
+
+    var canvasContext = canvas.getContext("2d");
+
+    var WIDTH;
+    var HEIGHT;
+
+    function visualize() {
+        WIDTH = canvas.width;
+        HEIGHT = canvas.height;
+
+        const drawFrequency = () => {
+            const bufferLengthAlt = analyser.frequencyBinCount;
+            const dataArrayAlt = new Uint8Array(bufferLengthAlt);
+
+            canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
+
+            const drawAlt = () => {
+                const drawVisual = requestAnimationFrame(drawAlt);
+
+                analyser.getByteFrequencyData(dataArrayAlt);
+
+                canvasContext.fillStyle = 'rgb(255, 255, 255)';
+                canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
+
+                const barWidth = (WIDTH / bufferLengthAlt) * 2.5;
+                let x = 0;
+
+                for (let i = 0; i < bufferLengthAlt; i++) {
+                    const barHeight = dataArrayAlt[i];
+
+                    let red, green;
+
+                    if (barHeight < 128) {
+                        red = 255;
+                        green = barHeight;
+                    } else {
+                        red = 255 - barHeight;
+                        green = 255;
+                    }
+                    let blue = 100;
+
+                    canvasContext.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+                    canvasContext.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+
+                    x += barWidth + 1;
+                }
+            };
+
+            drawAlt();
+        };
+
+        drawFrequency();
+
+        const drawNote = () => {
+            const drawVisual = requestAnimationFrame(drawNote);
+            const bufferLength = analyser.fftSize;
+            const buffer = new Float32Array(bufferLength);
+            analyser.getFloatTimeDomainData(buffer);
+            const pitch = autoCorrelate(buffer, audioContext.sampleRate);
+
+            let pitchDisplay = 'Too quiet...';
+            let noteDisplay = 'Too quiet...';
+            if (pitch !== -1) {
+                const roundedPitch = Math.round(pitch);
+                pitchDisplay = `Pitch: ${roundedPitch} Hz`;
+
+                const noteIndex = noteFromPitch(pitch) % 12;
+                noteDisplay = `Note: ${noteStrings[noteIndex]}`;
+            }
+
+            document.getElementById('pitch').innerText = pitchDisplay;
+            document.getElementById('note').innerText = noteDisplay;
+        };
+
+        drawNote();
+    }
+}
+
+  
+
   
   // AutoCorrelate from https://github.com/cwilso/PitchDetect/pull/23
   
